d905fc61a2a65eafa450dee7eee9445dd82c6be7
marco76tv <marco.sottana@gmail.com>
2023-06-20 11:07:56 +0200

    .

--- Filament/Resources/UserResource.php
+++ Filament/Resources/UserResource.php
@@ -1,31 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\User\Filament\Resources;
 
-use Modules\User\Filament\Resources\UserResource\Pages;
-use Modules\User\Filament\Resources\UserResource\RelationManagers;
-use Modules\LU\Models\User;
 use Filament\Forms;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Savannabits\FilamentModules\Concerns\ContextualResource;
 use Illuminate\Validation\Rules\Password;
+use Modules\User\Filament\Resources\UserResource\Pages;
 use Modules\User\Models\Role;
+use Modules\User\Models\User;
+use Savannabits\FilamentModules\Concerns\ContextualResource;
 
-class UserResource extends Resource
-{
+class UserResource extends Resource {
     use ContextualResource;
 
     protected static ?string $model = User::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-users';
 
-    public static function form(Form $form): Form
-    {
+    public static function form(Form $form): Form {
         return $form
             ->schema([
                 Forms\Components\TextInput::make('name')
@@ -36,13 +33,13 @@ class UserResource extends Resource
                     ->required()
                     ->maxLength(255)
                     ->unique(),
-                //Forms\Components\DateTimePicker::make('email_verified_at'),
+                // Forms\Components\DateTimePicker::make('email_verified_at'),
                 Forms\Components\TextInput::make('password')
                     ->password()
                     ->required()
                     ->maxLength(255)
                     ->rule(Password::default()),
-                    /*
+                /*
                 Forms\Components\TextInput::make('role_id')
                     ->required(),
                 Forms\Components\TextInput::make('display_name')
@@ -57,27 +54,26 @@ class UserResource extends Resource
             ]);
     }
 
-    public static function table(Table $table): Table
-    {
+    public static function table(Table $table): Table {
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('name')
                     ->searchable(),
 
-                //Tables\Columns\TextColumn::make('email'),
-                //Tables\Columns\TextColumn::make('email_verified_at')
+                // Tables\Columns\TextColumn::make('email'),
+                // Tables\Columns\TextColumn::make('email_verified_at')
                 //    ->dateTime(),
                 Tables\Columns\TextColumn::make('role.name'),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime(),
-                //Tables\Columns\TextColumn::make('updated_at')
+                // Tables\Columns\TextColumn::make('updated_at')
                 //    ->dateTime(),
-                //Tables\Columns\TextColumn::make('role_id'),
-                //Tables\Columns\TextColumn::make('display_name'),
-                //Tables\Columns\TextColumn::make('phone_number'),
-                //Tables\Columns\TextColumn::make('phone_verified_at')
+                // Tables\Columns\TextColumn::make('role_id'),
+                // Tables\Columns\TextColumn::make('display_name'),
+                // Tables\Columns\TextColumn::make('phone_number'),
+                // Tables\Columns\TextColumn::make('phone_verified_at')
                 //    ->dateTime(),
-                //Tables\Columns\TextColumn::make('photo'),
+                // Tables\Columns\TextColumn::make('photo'),
             ])
             ->filters([
                 Tables\Filters\SelectFilter::make('role')
@@ -107,18 +103,18 @@ class UserResource extends Resource
                         Forms\Components\TextInput::make('new_password_confirmation')
                             ->password()
                             ->label('Confirm New Password')
-                            ->rule('required', fn ($get) => ! ! $get('new_password'))
+                            ->rule('required', fn ($get) => (bool) $get('new_password'))
                             ->same('new_password'),
                     ])
                     ->icon('heroicon-o-key')
-                    //->visible(fn (User $record): bool => $record->role_id === Role::ROLE_ADMINISTRATOR)
-                    ,
+                // ->visible(fn (User $record): bool => $record->role_id === Role::ROLE_ADMINISTRATOR)
+                ,
                 Tables\Actions\Action::make('deactivate')
                     ->color('danger')
                     ->icon('heroicon-o-trash')
                     ->action(fn (User $record) => $record->delete())
-                    //->visible(fn (User $record): bool => $record->role_id === Role::ROLE_ADMINISTRATOR)
-                    ,
+                // ->visible(fn (User $record): bool => $record->role_id === Role::ROLE_ADMINISTRATOR)
+                ,
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
@@ -126,19 +122,16 @@ class UserResource extends Resource
             ->defaultSort('created_at', 'desc');
     }
 
-    public static function getRelations(): array
-    {
+    public static function getRelations(): array {
         return [
-            //
         ];
     }
 
-    public static function getPages(): array
-    {
+    public static function getPages(): array {
         return [
             'index' => Pages\ListUsers::route('/'),
             'create' => Pages\CreateUser::route('/create'),
-            //'edit' => Pages\EditUser::route('/{record}/edit'),
+            // 'edit' => Pages\EditUser::route('/{record}/edit'),
         ];
     }
 }
