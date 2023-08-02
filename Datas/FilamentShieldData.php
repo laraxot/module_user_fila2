5d6abb8b7ed129b88f847d910e71ffc8f3d4e245
marco xot <marco.sottana@gmail.com>
2023-08-02 12:47:22 +0200

    up

--- Datas/FilamentShieldData.php
+++ Datas/FilamentShieldData.php
@@ -4,30 +4,21 @@ declare(strict_types=1);
 
 namespace Modules\User\Datas;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Auth;
 use Spatie\LaravelData\Data;
 use Webmozart\Assert\Assert;
 
 /**
  * Undocumented class.
  */
-class FilamentShieldData extends Data
-{
+class FilamentShieldData extends Data {
     public ShieldResourceData $shield_resource;
 
     public SuperAdminData $super_admin;
     public FilamentUserData $filament_user;
 
-    public static function make(): self
-    {
-        $xot = config('filament-shield');
-
-        if (! is_array($xot)) {
-            dddx($xot);
-        }
+    public static function make(): self {
+        Assert::isArray($xot = config('filament-shield'), 'check config [filament-shield] ');
 
         return self::from($xot);
     }
-
 }
