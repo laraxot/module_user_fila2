1883e4261af74ef34f0467ef3d6e501425d9676d
marco xot <marco.sottana@gmail.com>
2023-08-22 13:12:40 +0200

    up

--- Filament/Pages/Dashboard.php
+++ Filament/Pages/Dashboard.php
@@ -7,10 +7,9 @@ namespace Modules\User\Filament\Pages;
 use Filament\Pages\Page;
 use Savannabits\FilamentModules\Concerns\ContextualPage;
 
-class Dashboard extends Page
-{
+class Dashboard extends Page {
     use ContextualPage;
-    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static ?string $navigationIcon = 'heroicon-o-home';
 
     protected static string $view = 'user::filament.pages.dashboard';
 }
--- Filament/Resources/UserResource.php
+++ Filament/Resources/UserResource.php
@@ -32,8 +32,7 @@ use Modules\User\Models\Role;
 use Modules\User\Models\User;
 use Savannabits\FilamentModules\Concerns\ContextualResource;
 
-class UserResource extends Resource
-{
+class UserResource extends Resource {
     use ContextualResource;
     protected static ?string $model = User::class;
 
@@ -70,20 +69,21 @@ class UserResource extends Resource
     }
     */
 
-    public static function getWidgets(): array
-    {
+    protected static function getNavigationBadge(): ?string {
+        return strval(static::getModel()::count());
+    }
+
+    public static function getWidgets(): array {
         return [
             Widgets\UserOverview::class,
         ];
     }
 
-    public static function extendForm(\Closure $callback): void
-    {
+    public static function extendForm(\Closure $callback): void {
         static::$extendFormCallback = $callback;
     }
 
-    public static function formOld(Form $form): Form
-    {
+    public static function formOld(Form $form): Form {
         return $form
             ->schema([
                 Forms\Components\TextInput::make('name')
@@ -100,8 +100,7 @@ class UserResource extends Resource
             ]);
     }
 
-    public static function form(Form $form): Form
-    {
+    public static function form(Form $form): Form {
         return $form
             ->schema(function () {
                 $schema = [
@@ -159,8 +158,7 @@ class UserResource extends Resource
             ->columns(12);
     }
 
-    public static function table(Table $table): Table
-    {
+    public static function table(Table $table): Table {
         return $table
             ->columns([
                 TextColumn::make('id')->sortable(),
@@ -239,8 +237,7 @@ class UserResource extends Resource
             ->defaultSort('created_at', 'desc');
     }
 
-    public static function enablePasswordUpdates(bool|\Closure $condition = true): void
-    {
+    public static function enablePasswordUpdates(bool|\Closure $condition = true): void {
         static::$enablePasswordUpdates = $condition;
     }
 
@@ -251,8 +248,7 @@ class UserResource extends Resource
     }
     */
 
-    public static function getRelations(): array
-    {
+    public static function getRelations(): array {
         return [
             RelationManagers\TeamsRelationManager::class,
             RelationManagers\ProfileRelationManager::class,
@@ -260,8 +256,7 @@ class UserResource extends Resource
         ];
     }
 
-    public static function getPages(): array
-    {
+    public static function getPages(): array {
         return [
             'index' => Pages\ListUsers::route('/'),
             'create' => Pages\CreateUser::route('/create'),
