c8c358f051bcbf9610062ed688233f461ea9a7d8
marco76tv <marco76tv@users.noreply.github.com>
2023-08-23 15:14:43 +0000

    Check & fix styling

--- Filament/Resources/TeamResource/Pages/CreateTeam.php
+++ Filament/Resources/TeamResource/Pages/CreateTeam.php
@@ -1,9 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\User\Filament\Resources\TeamResource\Pages;
 
-
-use Filament\Pages\Actions;
 use Filament\Resources\Pages\CreateRecord;
 use Modules\User\Filament\Resources\TeamResource;
 
